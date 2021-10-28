ad50fd400821a8054f6a77c2d53a332e6ba2cdd3
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-10-27 18:34:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+video = createCapture(VIDEO);
+video.size(550, 500);
+
+canvas = createCanvas(550, 550);
+canvas.position(560, 150);
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+background('#FF0000');
+}
+
+function modelLoaded() {
+console.log('PoseNet is Initialized!');
+}
+
+function gotPoses(results) {
+if(results.length > 0) {
+console.log(results);
+ }
+}

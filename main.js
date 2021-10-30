b181ef14fc8759ff74df851ef229f93699e4fecd
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-10-29 18:40:55 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
 video = createCapture(VIDEO);
 video.size(550, 500);
@@ -7,10 +12,15 @@ canvas.position(560, 150);
 
 poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
-}
+} 
 
 function draw() {
-background('#FF0000');
+background('lightgreen');
+
+document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+fill('lightblue');
+stroke('lightblue')
+square(noseX, noseY, difference);
 }
 
 function modelLoaded() {
@@ -20,5 +30,14 @@ console.log('PoseNet is Initialized!');
 function gotPoses(results) {
 if(results.length > 0) {
 console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("noseX = " + noseX +" noseY = " + noseY);
+
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX = results[0].pose.rightWrist.x;
+difference = floor (leftWristX - rightWristX);
+
+console.log("leftWristx = " + leftWristX + " rightWristX "+ rightWristX + " difference" + difference);
  }
 }
